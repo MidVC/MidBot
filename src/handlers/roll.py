94de708515b48c8fa63495b4fca829b37f245d89
MidVC <lutony040720@gmail.com>
2024-01-08 18:31:00 -0500

    added functionality !roll

---
+++ src/handlers/roll.py
@@ -0,0 +1,4 @@
+import random
+
+def handle_roll(range: int) -> str:
+    return str(random.randint(1, range))
--- src/responses.py
+++ src/responses.py
@@ -1,7 +1,17 @@
+from .handlers.roll import handle_roll
+
 def handle_response(message) -> str:
     lc_message = message.lower()
 
     if lc_message == '>rs':
         return 'deranker!'
+
+    words = message.split(' ')
+    
+    if words[0] == '!roll':
+        if len(words) == 1 or not words[1].isnumeric():
+            return handle_roll(100)
+        else:
+            return handle_roll(int(words[1]))
 
     return ''
