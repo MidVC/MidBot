f5250090e0e86789cb7b6a4ecf022fb5c916e91d
MidVC <lutony040720@gmail.com>
2024-01-08 23:27:49 -0500

    fixed >rs accuracy visualization bug

--- src/handlers/rs.py
+++ src/handlers/rs.py
@@ -6,6 +6,6 @@ def handle_rs(user_name: str, clientId, clientSecret) -> str :
     scoreList = api.user_scores(user_id, "recent", include_fails=True, mode="osu", limit=10, offset=0)
     return str(scoreList[0].pp) + "pp on " + \
             scoreList[0].beatmapset.title + " +" + \
-            str(scoreList[0].mods) + ", " + str(scoreList[0].accuracy) + "%, " + \
+            str(scoreList[0].mods) + ", " + str(scoreList[0].accuracy * 100) + "%, " + \
             str(scoreList[0].max_combo) + "x"
            
