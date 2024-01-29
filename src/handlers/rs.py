59578ba9b9ca77af48586ea1b306e9137b522f67
MidVC <lutony040720@gmail.com>
2024-01-29 11:04:44 -0500

    adjusted rs format

--- src/handlers/rs.py
+++ src/handlers/rs.py
@@ -4,8 +4,8 @@ def handle_rs(user_name: str, clientId, clientSecret) -> str :
     api = Ossapi(clientId, clientSecret)
     user_id = api.user(user=user_name).id
     scoreList = api.user_scores(user_id, "recent", include_fails=True, mode="osu", limit=10, offset=0)
-    return str(scoreList[0].pp) + "pp on " + \
+    return "{:.2f}".format(scoreList[0].pp) + "pp on " + \
             scoreList[0].beatmapset.title + " +" + \
-            str(scoreList[0].mods) + ", " + str(scoreList[0].accuracy * 100) + "%, " + \
+            str(scoreList[0].mods) + ", " + "{:.2f}".format(scoreList[0].accuracy * 100) + "%, " + \
             str(scoreList[0].max_combo) + "x"
            
