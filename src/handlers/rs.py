b931276c556affa6df10fa360522b67f8253301d
MidVC <lutony040720@gmail.com>
2024-01-29 21:57:37 -0500

    added check for unknown user

--- src/handlers/rs.py
+++ src/handlers/rs.py
@@ -2,10 +2,19 @@ from ossapi import Ossapi
 
 def handle_rs(user_name: str, clientId, clientSecret) -> str :
     api = Ossapi(clientId, clientSecret)
-    user_id = api.user(user=user_name).id
+
+    # check user not found
+    try:
+        user_id = api.user(user=user_name).id
+    except:
+        return "User " + user_name + " not found!"
+    
     scoreList = api.user_scores(user_id, "recent", include_fails=True, mode="osu", limit=10, offset=0)
+
+    # check no recent score
     if len(scoreList) == 0:
         return "User " + user_name + " does not have a recent play on bancho!"
+    
     return ("None pp on " if scoreList[0].pp == None else "{:.2f}".format(scoreList[0].pp) + "pp on ") + \
             scoreList[0].beatmapset.title + " +" + \
             str(scoreList[0].mods) + ", " + \
