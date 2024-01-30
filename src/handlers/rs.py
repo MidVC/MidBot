8d686def1d0afcd6a23b6bc3f619dce3a0fd0f82
MidVC <lutony040720@gmail.com>
2024-01-29 21:44:06 -0500

    fixed nonepp bug

--- src/handlers/rs.py
+++ src/handlers/rs.py
@@ -4,8 +4,9 @@ def handle_rs(user_name: str, clientId, clientSecret) -> str :
     api = Ossapi(clientId, clientSecret)
     user_id = api.user(user=user_name).id
     scoreList = api.user_scores(user_id, "recent", include_fails=True, mode="osu", limit=10, offset=0)
-    return "{:.2f}".format(scoreList[0].pp) + "pp on " + \
+    return ("None pp on " if scoreList[0].pp == None else "{:.2f}".format(scoreList[0].pp) + "pp on ") + \
             scoreList[0].beatmapset.title + " +" + \
-            str(scoreList[0].mods) + ", " + "{:.2f}".format(scoreList[0].accuracy * 100) + "%, " + \
+            str(scoreList[0].mods) + ", " + \
+            "{:.2f}".format(scoreList[0].accuracy * 100) + "%, " + \
             str(scoreList[0].max_combo) + "x"
            
